b1e52d1498bcb81cce6c7abb08b9fcd44ffd8680
Your Name <you@example.com>
2024-01-16 06:24:05 -0800

    getUserById,getBlogById,getAllBlog

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAuthenticated } from '../middleware/auth.js';
-import {createBlog,myBlog,updateBlog,deleteBlog} from '../controllers/blog.js'
+import {createBlog,myBlog,updateBlog,deleteBlog,getAllBlogs,getBlogById} from '../controllers/blog.js'
 
 const router=express.Router();
 
@@ -8,6 +8,10 @@ router.post('/new',isAuthenticated,createBlog);
 router.get('/myblogs',isAuthenticated,myBlog);
 router.put('/:id',isAuthenticated,updateBlog);
 router.delete('/:id',isAuthenticated,deleteBlog);
+
+router.get('/allblogs',getAllBlogs);
+
+router.get('/blog/:id',isAuthenticated,getBlogById); 
 
 
 export default router
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAuthenticated } from '../middleware/auth.js';
-import {logout,userLogin,userRegister,getMyProfile} from '../controllers/user.js'
+import {logout,userLogin,userRegister,getMyProfile,getUserById} from '../controllers/user.js'
 
 const router=express.Router();
 
@@ -14,4 +14,7 @@ const router=express.Router();
    router.get('/logout',logout)
 
    router.get('/myprofile',isAuthenticated,getMyProfile)
+
+   router.get('/:id',getUserById);
+
    export default router
